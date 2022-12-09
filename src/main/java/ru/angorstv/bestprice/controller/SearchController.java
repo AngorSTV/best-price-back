1f3f62d4708251c4cccb6ccc39c553b42d194687
Andrey Gornostaev <angor@inbox.ru>
2022-12-09 17:22:11 +0300

    paging, sorting case insensitive

--- src/main/java/ru/angorstv/bestprice/controller/SearchController.java
+++ src/main/java/ru/angorstv/bestprice/controller/SearchController.java
@@ -3,6 +3,7 @@ package ru.angorstv.bestprice.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,23 +15,21 @@ import ru.angorstv.bestprice.common.Sorting;
 import ru.angorstv.bestprice.entity.Product;
 import ru.angorstv.bestprice.service.SearchService;
 
-import java.util.List;
-
 @Slf4j
 @RestController
 @RequestMapping("/api")
 public class SearchController {
-
+    
     private final SearchService searchService;
     private final ObjectMapper objectMapper;
-
+    
     public SearchController(SearchService searchService, ObjectMapper objectMapper) {
         this.searchService = searchService;
         this.objectMapper = objectMapper;
     }
-
+    
     @GetMapping("/search")
-    public ResponseEntity<List<Product>> search(@RequestParam String term,
+    public ResponseEntity<Page<Product>> search(@RequestParam String term,
                                                 @RequestParam(required = false) Sorting sort,
                                                 @RequestParam(required = false) Direct direct,
                                                 @RequestParam(required = false) Integer page,
--- src/main/java/ru/angorstv/bestprice/service/SearchService.java
+++ src/main/java/ru/angorstv/bestprice/service/SearchService.java
@@ -1,9 +1,11 @@
 package ru.angorstv.bestprice.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import ru.angorstv.bestprice.common.Direct;
 import ru.angorstv.bestprice.common.SearchRequest;
 import ru.angorstv.bestprice.entity.Product;
 import ru.angorstv.bestprice.harvester.AliHarvester;
@@ -17,21 +19,21 @@ import java.util.List;
 
 @Service
 public class SearchService {
-
+    
     private final WildberriesHarvester wildberriesHarvester;
     private final AliHarvester aliHarvester;
     private final OzonHarvester ozonHarvester;
     private final ProductRepository productRepository;
-
+    
     public SearchService(WildberriesHarvester wildberriesHarvester, AliHarvester aliHarvester, OzonHarvester ozonHarvester, ProductRepository productRepository) {
         this.wildberriesHarvester = wildberriesHarvester;
         this.aliHarvester = aliHarvester;
         this.ozonHarvester = ozonHarvester;
         this.productRepository = productRepository;
     }
-
+    
     @Transactional
-    public List<Product> search(SearchRequest searchRequest) throws JsonProcessingException {
+    public Page<Product> search(SearchRequest searchRequest) throws JsonProcessingException {
         List<Product> products = new LinkedList<>();
         products.addAll(wildberriesHarvester.getProducts(searchRequest.getTerm()));
         //products.addAll(aliHarvester.getProducts(searchRequest.getTerm()));
@@ -39,17 +41,22 @@ public class SearchService {
         productRepository.saveAll(products);
         return getFromBd(searchRequest);
     }
-
-    private List<Product> getFromBd(SearchRequest searchRequest) {
+    
+    private Page<Product> getFromBd(SearchRequest searchRequest) {
         String term = searchRequest.getTerm();
-        int offset = searchRequest.getPage() * searchRequest.getPageSize();
-        int size = searchRequest.getPageSize();
         String field;
         switch (searchRequest.getSorting()) {
             case RELEVANT -> field = "name";
             case SHOP -> field = "shop";
             default -> field = "price";
         }
-        return productRepository.findByNamePaging(term, PageRequest.of(searchRequest.getPage(), searchRequest.getPageSize(), Sort.by(field).ascending())).getContent();
+        Sort sort;
+        if (searchRequest.getDirect().equals(Direct.ASC)) {
+            sort = Sort.by(field).ascending();
+        } else {
+            sort = Sort.by(field).descending();
+        }
+        PageRequest pageRequest = PageRequest.of(searchRequest.getPage(), searchRequest.getPageSize(), sort);
+        return productRepository.findByNamePaging(term, pageRequest);
     }
 }
